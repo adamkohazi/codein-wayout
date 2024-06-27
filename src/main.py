ed881dd12389e195b371975005a290d27fc94e74
adamkohazi <kohazi.adam@gmail.com>
2024-06-27 17:09:23 +0200

    Level 1 solver

--- src/main.py
+++ src/main.py
@@ -1,28 +1,25 @@
 import sys
 import xml.etree.ElementTree as ET
 from enum import Enum
-from dataclasses import dataclass
-from collections import namedtuple
 from typing import NamedTuple
+import copy
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 class Coords(NamedTuple):
     x: int
     y: int
 
-class Branch(NamedTuple):
-    actions: list
-    currentPos: Coords
+    def __add__(self, step: 'Step'):
+        return Coords(x = (self.x + step.cellNumber * step.direction.vector.x), y = (self.y + step.cellNumber * step.direction.vector.y))
+    
+    def surrounding(self):
+        return [Coords(x = (self.x + direction.vector.x), y = (self.y + direction.vector.y)) for direction in Directions]
 
 class Direction(NamedTuple):
     name: str
     code: str
     vector: Coords
-
-class Step(NamedTuple):
-    direction: Direction
-    cellNumber: int
 
 class Directions(Enum):
     LEFT  = Direction(name='left' , code='1', vector=Coords(x=-1,y= 0))
@@ -30,151 +27,215 @@ class Directions(Enum):
     UP    = Direction(name='up'   , code='3', vector=Coords(x= 0,y=-1))
     DOWN  = Direction(name='down' , code='4', vector=Coords(x= 0,y= 1))
 
-# Prints the maze to the console
-def printMaze(maze):
-    for row in maze:
-        print(' '.join([row[x] for x in range(17)]))
+class Step(NamedTuple):
+    direction: Direction
+    cellNumber: int
 
-# Checks if a point in the maze is a path
-def isPath(maze, coords):
-    if coords.x < 0 or 17<=coords.x or coords.y < 0 or 17<=coords.y:
+class Branch(NamedTuple):
+    actions: list
+    currentPos: Coords
+
+    def toXml(self):
+        actionsRoot = ET.Element("Actions")
+        for action in self.actions:
+            if type(action) is Step:
+                stepElement = ET.SubElement(actionsRoot, "Step")
+                ET.SubElement(stepElement, "Direction").text = action.direction.name
+                ET.SubElement(stepElement, "CellNumber").text = str(action.cellNumber)
+        
+        actionsTree = ET.ElementTree(actionsRoot)
+        ET.indent(actionsTree, space="\t")
+        return ET.tostring(actionsRoot, encoding='unicode')
+
+class Maze(object):
+    """Class for keeping track of and interacting with a maze.
+    """
+
+    # Project constants
+    WIDTH = 17
+    HEIGHT = 17
+
+    def __init__(self, mazeXml):
+        # Parse maze XML
+        mazeRoot = ET.fromstring(mazeXml)
+
+        self.level = int(mazeRoot.find('./Level').text)
+
+        startRow = int(mazeRoot.find('./StartPoint/Row').text)-1
+        startColumn = int(mazeRoot.find('./StartPoint/Column').text)-1
+        self.startCoords = Coords(startColumn, startRow)
+
+        escapeRow = int(mazeRoot.find('./EscapePoint/Row').text)-1
+        escapeColumn = int(mazeRoot.find('./EscapePoint/Column').text)-1
+        self.escapeCoords = Coords(escapeColumn, escapeRow)
+
+        # Print the row value
+        if DEBUG_MODE:
+            print("Level: ", self.level)
+            print("Start: ", self.startCoords)
+            print("Escape: ", self.escapeCoords)
+
+        # Parse the walls
+        # Start blank, fixed 17x17
+        self.grid = [[' ' for x in range(17)] for y in range(17)]
+
+        # Set the top and bottom walls
+        for n in range(self.WIDTH):
+            self.grid[ 0][ n] = 'x'
+            self.grid[self.HEIGHT-1][ n] = 'x'
+        
+        # Set the left and right walls
+        for n in range(self.WIDTH):
+            self.grid[ n][ 0] = 'x'
+            self.grid[ n][self.HEIGHT-1] = 'x'
+
+        # Set start and escape points
+        self.grid[startRow][startColumn] = 's'
+        self.grid[escapeRow][escapeColumn] = 'e'
+
+        # Set inside walls
+        for wall in mazeRoot.findall('./InsideItems/Wall'):
+            x = int(wall.find('Row').text)-1
+            y = int(wall.find('Column').text)-1
+            self.grid[y][x] = 'x'
+
+        # Set traps
+        for trap in mazeRoot.findall('./InsideItems/Trap'):
+            x = int(trap.find('Row').text)-1
+            y = int(trap.find('Column').text)-1
+            self.grid[y][x] = 'h'
+
+        # Visualize the result
+        if DEBUG_MODE:
+            self.print()
+
+    # Prints the maze to the console
+    def print(self):
+        for row in  self.grid:
+            print(' '.join([row[x] for x in range(self.WIDTH)]))
+
+    # Checks if a point in the maze is a path. Traps are considered as path, their effect is handled later.
+    def isPath(self, coords):
+        if coords.x < 0 or self.WIDTH<=coords.x or coords.y < 0 or self.HEIGHT<=coords.y:
+            return False
+        tile = self.grid[coords.y][coords.x]
+        if tile == ' ' or tile == 's' or tile == 'e' or tile == 'h':
+            return True
         return False
-    tile = maze[coords.y][coords.x]
-    if tile == ' ' or tile == 's' or tile == 'e':
-        return True
-    return False
 
-# Returns the 4 surrounding points around a given coordinate
-def surroundingCoords(coords):
-    return [Coords(x = (coords.x + direction.vector.x), y = (coords.y + direction.vector.y)) for direction in Directions]
+    # Checks if a point in the maze is a trap.
+    def isTrap(self, coords):
+        return self.grid[coords.y][coords.x] == 'h'
 
-# Counts the number of paths leading to a point
-def countPaths(maze, coords):
-    # Sanity check
-    if DEBUG_MODE:
-        if not isPath(maze, coords):
-            raise ValueError("Can't find options from an invalid position.")
+    # Counts the number of paths leading to a point
+    def countPaths(self, coords):
+        # Sanity check
+        if DEBUG_MODE:
+            if not self.isPath(coords):
+                raise ValueError("Can't find options from an invalid position.")
+        
+        return sum([self.isPath(coord) for coord in coords.surrounding()])
+
+    # Returns a list of "good" options for moving
+    def findSteps(self, coords):
+        # Sanity check
+        if DEBUG_MODE:
+            if not self.isPath(coords):
+                raise ValueError("Can't find options from an invalid position.")
+
+        options = []
+
+        # Check all directions radially
+        for radialDirection in [d.value for d in Directions]:
+            # Virtually walk until a wall (or trap) is hit
+            distance = 1
+            while(True):
+                # Update virtual position
+                midPoint = coords + Step(direction = radialDirection, cellNumber = distance)
+                # Stop if we've hit a wall
+                if(not self.isPath(midPoint)):
+                    break
+
+                # Check if projected position has paths joining sideways (corner or intersection)
+                if any([self.isPath(midPoint + Step(direction=direction, cellNumber=1))
+                        for direction in [d.value for d in Directions]
+                        if direction.vector.x!=radialDirection.vector.x and direction.vector.y!=radialDirection.vector.y]) or self.grid[midPoint.y][midPoint.x]=='e':
+                    options.append(Step(radialDirection, distance))
+
+                # Go one further
+                distance += 1
+        return options
     
-    return sum([isPath(maze, coord) for coord in surroundingCoords(coords)])
+    def deactivateTrap(self, startCoords, step):
+        for distance in range(step.cellNumber)+1:
+            midPoint = startCoords + Step(direction = step.direction, cellNumber = distance)
+            if self.isTrap(midPoint):
+                self.grid[midPoint.y][midPoint.x] = ' '
+                return True
+        return False
 
+    def findShortestPath(self, startCoords=None, endCoords=None, modifyMaze=False):
+        # Use start and escape points of maze as default
+        if startCoords is None:
+            startCoords = self.startCoords
+        if endCoords is None:
+            endCoords = self.escapeCoords
 
-# Move a point with a given step
-def move(maze, startPoint, step):
-    endPoint = Coords(x = (startPoint.x + step.cellNumber * step.direction.vector.x), y = (startPoint.y + step.cellNumber * step.direction.vector.y))
-    
-    # Sanity check
-    if DEBUG_MODE:
-        for i in range(step.cellNumber):
-            midPoint = Coords(x = (startPoint.x + i * step.direction.vector.x), y = (startPoint.y + i * step.direction.vector.y))
-            if not isPath(maze, midPoint):
-                raise ValueError("Can't move throught invalid points.")
+        # Only information in the beginning is that start can be reached in 0 steps.
+        branches = [Branch(actions=[], currentPos=startCoords)]
 
-    return endPoint
+        # Flags to indicate status
+        endCoordReached = False
+        anyImprovement = True
 
-# Returns a list of "good" options for moving
-def findOptions(maze, coords):
-    # Sanity check
-    if DEBUG_MODE:
-        if not isPath(maze, coords):
-            raise ValueError("Can't find options from an invalid position.")
+        # Update every branch with new moves
+        while((not endCoordReached) and anyImprovement):
+            # Reset flag every iteration
+            anyImprovement = False
 
-    options = []
+            # Update all branches
+            for branch in branches[:]:
+                # Evaluate all valid steps
+                for step in self.findSteps(branch.currentPos):
+                    # Check if we cross or land on a trap:
+                    if self.deactivateTrap(branch.currentPos, step):
+                        newPos = self.startCoords
+                    else:
+                        newPos = branch.currentPos + step
+                    newActions = branch.actions.copy()
+                    newActions.append(step)
 
-    # Check all directions radially
-    for radialDirection in [d.value for d in Directions]:
-        # Virtually walk until a wall (or trap) is hit
-        distance = 1
-        while(True):
-            # Update virtual position
-            midPoint = Coords(x = (coords.x + distance * radialDirection.vector.x), y = (coords.y + distance * radialDirection.vector.y))
-            # Stop if we've hit a wall
-            if(not isPath(maze, midPoint)):
-                break
-
-            # Check if projected position has paths joining sideways (corner or intersection)
-            if any([isPath(maze, Coords(x = (midPoint.x + direction.vector.x), y = (midPoint.y + direction.vector.y)))
-                    for direction in [d.value for d in Directions]
-                    if direction.vector.x!=radialDirection.vector.x and direction.vector.y!=radialDirection.vector.y]) or maze[midPoint.y][midPoint.x]=='e':
-                options.append(Step(radialDirection, distance))
-
-            # Go one further
-            distance += 1
-    
-    return options   
-
-# Parse maze XML
-#mazeXml = sys.argv[1]
-with open('./test/example_maze_2.txt', 'r') as file:
-    mazeXml = file.read().replace('\n', '')
-
-mazeRoot = ET.fromstring(mazeXml)
-
-level = int(mazeRoot.find('./Level').text)
-
-startRow = int(mazeRoot.find('./StartPoint/Row').text)-1
-startColumn = int(mazeRoot.find('./StartPoint/Column').text)-1
-startCoords = Coords(startColumn, startRow)
-
-escapeRow = int(mazeRoot.find('./EscapePoint/Row').text)-1
-escapeColumn = int(mazeRoot.find('./EscapePoint/Column').text)-1
-escapeCoords = Coords(escapeColumn, escapeRow)
-
-# Print the row value
-print("Level: ", level)
-print("Start: ", startCoords)
-print("Escape: ", escapeCoords)
-
-# Parse the walls
-# Start blank, fixed 17x17
-maze = [[' ' for x in range(17)] for y in range(17)]
-
-# Set the outer walls
-for n in range(17):
-    maze[ 0][ n] = 'x' # Top edge
-    maze[16][ n] = 'x' # Bottom edge
-    maze[ n][ 0] = 'x' # Left edge
-    maze[ n][16] = 'x' # Right edge
-
-# Set start and escape points
-maze[startRow][startColumn] = 's'
-maze[escapeRow][escapeColumn] = 'e'
-
-# Set inside walls
-for wall in mazeRoot.findall('./InsideItems/Wall'):
-    x = int(wall.find('Row').text)-1
-    y = int(wall.find('Column').text)-1
-    maze[y][x] = 'x'
-
-# Set traps
-for trap in mazeRoot.findall('./InsideItems/Trap'):
-    x = int(trap.find('Row').text)-1
-    y = int(trap.find('Column').text)-1
-    maze[y][x] = 'h'
-
-printMaze(maze)
-
-print(findOptions(maze, Coords(15,6)))
-
-branches = [Branch(actions=[], currentPos=startCoords)]
-
-# Update every branch with new moves
-finished = False
-while(not finished):
-    for branch in branches[:]:
-        for step in findOptions(maze, branch.currentPos):
-            newPos = Coords(x = (branch.currentPos.x + step.cellNumber * step.direction.vector.x), y = (branch.currentPos.y + step.cellNumber * step.direction.vector.y))
-            newActions = branch.actions.copy()
-            newActions.append(step)
+                    moveCount = len(branch.actions) + 1
+                    
+                    # If position was never reached
+                    if newPos not in [b.currentPos for b in branches[:]]:
+                        newBranch = Branch(newActions, newPos)
+                        branches.append(newBranch)
+                        anyImprovement = True
+                    
+                        if newPos == endCoords:
+                            endCoordReached = True
+                    
+        
+        if DEBUG_MODE:
+            for branch in branches:
+                self.grid[branch.currentPos.y][branch.currentPos.x] = chr(65+len(branch.actions))
             
-            moveCount = len(branch.actions) + 1
-            # If position was never reached
-            if newPos not in [b.currentPos for b in branches[:]]:
-                branches.append(Branch(newActions, newPos))
-            
-            if newPos == escapeCoords:
-                finished = True
-    
-for branch in branches[:]:
-    maze[branch.currentPos.y][branch.currentPos.x] = chr(65+len(branch.actions))
+            self.print()
+        
+        for branch in branches:
+            if branch.currentPos == endCoords:
+                return branch
 
-printMaze(maze)
+        print("Oh no...No branches are reaching the end coordinates")
+
+    def findShortestPathTraps(self, startCoords=None, endCoords=None):
+        # Make a copy of the grid, we'll need to modify it due to traps.
+        currentMaze = copy.copy(self)
+
+        return currentMaze.findShortestPath(startCoords, endCoords, modifyMaze=True)
+
+# Init maze from command line argument
+maze = Maze(sys.argv[1])
+if(maze.level == 1):
+    print(maze.findShortestPath().toXml())
